d7f298b2d48d65a22e01c90f95e50415b8741f11
Michael Irby <michael.irby@dfinsolutions.com>
2023-11-02 09:43:23 -0400

    init commit of basic formik

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View, Button } from 'react-native';
+import { Formik } from 'formik';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Formik
+        initialValues={{ title: '', name: '', phone: '' }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {(fProps) => (
+          <View>
+            <TextInput
+              style={styles.input}
+              placeholder='Review title'
+              onChangeText={fProps.handleChange('title')}
+              value={fProps.values.title}
+            />
+            <TextInput
+              multiline
+              style={styles.input}
+              placeholder='Name'
+              onChangeText={fProps.handleChange('name')}
+              value={fProps.values.name}
+            />
+            <TextInput
+              style={styles.input}
+              placeholder='Phone'
+              onChangeText={fProps.handleChange('phone')}
+              value={fProps.values.phone}
+            />
+            <Button
+              title='submit'
+              color='maroon'
+              onPress={fProps.handleSubmit}
+            />
+          </View>
+        )}
+      </Formik>
     </View>
   );
 }
@@ -16,5 +49,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    padding: 10,
+    fontSize: 18,
+    borderRadius: 6,
   },
 });
